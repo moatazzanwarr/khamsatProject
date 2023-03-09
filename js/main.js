4520a0cc3a2c3daf489cd5b597b46f0978729a39
moatazzanwarr <moatazzanwarr@gmail.com>
2023-03-09 15:22:08 +0200

    edit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+let sidemenue = document.getElementById("sidemenue");
+
+function openBtn(){
+    sidemenue.style.width = "300px";
+};
+
+function closeBtn(){
+    sidemenue.style.width = "0px";
+};
